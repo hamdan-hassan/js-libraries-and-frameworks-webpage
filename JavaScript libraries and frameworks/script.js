ceab1c27a58ffc25dedc5594e5cc2ec454c3a266
Hamdan <hassanmohammad922@gmail.com>
2019-07-10 03:56:17 +0300

    initial commit

---
+++ JavaScript libraries and frameworks/script.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function (){
+	$(".dropdown-menu").on("click" , ".dropdown-item" , function(e) {
+
+    let txt = $(e.target).text();
+     $("#" + txt).addClass("highlight")
+
+     if ($("#" + txt).change()) {
+       setTimeout(removeHightlight , 5000)
+     }
+     
+     function removeHightlight() {
+         $("#" + txt).removeClass("highlight");
+     	
+     }
+
+     var winHeight = $(window).height(),
+    topOffset = $("#" + txt).offset().top,
+    elementHeight = $("#" + txt).height();
+  var top = topOffset - winHeight + elementHeight;
+
+     $('html, body').animate({scrollTop: top}, 3000);
+
+	});
+
+
+
+
+$("#up-arrow").click(function() {
+
+$("html, body").animate({ scrollTop: 0 }, 3000);
+
+
+});
+
+
+$(window).scroll(function() {
+
+
+if ($(this).scrollTop() > 40) {
+
+$("#up-arrow").fadeIn();
+
+}
+
+else {
+$("#up-arrow").fadeOut();
+
+
+}
+
+});
+
+
+
+});
+
+
